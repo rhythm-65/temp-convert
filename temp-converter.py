5b990df1ebf3cea12a6c47f588a7ff68d1239ab5
Mr.Nerd <118065878+rhythm-65@users.noreply.github.com>
2023-10-01 14:18:49 +0530

    Update temp-converter.py

--- temp-converter.py
+++ temp-converter.py
@@ -1,11 +1,23 @@
 print("Enter 1 to convert to Celsius and 2 to convert into fahrenheit:")
 choice=int(input())
 if choice==1:
-  fahrenheit = float(input("Enter temperature in Fahrenheit: "))
-  celsius = (fahrenheit - 32) * 5/9
-  print("Temperature in Celsius:", celsius)
+  while True:
+        fahrenheit_input = input("Enter temperature(s) in Fahrenheit (separated by commas): ")
+        if fahrenheit_input.lower() == "done":
+            break
+        fahrenheit_list = fahrenheit_input.split(",")
+        for fahrenheit in fahrenheit_list:
+            celsius = (float(fahrenheit) - 32) * 5/9
+            print(f"{fahrenheit} degrees Fahrenheit = {celsius} degrees Celsius")
+elif choice == 2:
+    while True:
+        celsius_input = input("Enter temperature(s) in Celsius (separated by commas): ")
+        if celsius_input.lower() == "done":
+            break
+        celsius_list = celsius_input.split(",")
+        for celsius in celsius_list:
+            fahrenheit = (float(celsius) * 9/5) + 32
+            print(f"{celsius} degrees Celsius = {fahrenheit} degrees Fahrenheit")
 else:
-  celsius=float(input("Enter the temperature in Celsius:"))
-  fahrenheit=(9*celsius/5)+32
-  print("Temperature in Fahrenheit:",fahrenheit)
+    print("Invalid choice")
               
