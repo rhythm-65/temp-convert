6e932321cfc5afe8e482d2743f81df7a5e73f2f6
SanyamB0912 <115093596+SanyamB0912@users.noreply.github.com>
2023-10-01 08:13:35 +0530

    Update temp-converter.py
    Added a choice to convert into degree or fahrenheit

--- temp-converter.py
+++ temp-converter.py
@@ -1,3 +1,11 @@
-fahrenheit = float(input("Enter temperature in Fahrenheit: "))
-celsius = (fahrenheit - 32) * 5/9
-print("Temperature in Celsius:", celsius)
+print("Enter 1 to convert to Celsius and 2 to convert into fahrenheit:")
+choice=int(input())
+if choice==1:
+  fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+  celsius = (fahrenheit - 32) * 5/9
+  print("Temperature in Celsius:", celsius)
+else:
+  celsius=float(input("Enter the temperature in Celsius:"))
+  fahrenheit=(9*celsius/5)+32
+  print("Temperature in Fahrenheit:",fahrenheit)
+              
